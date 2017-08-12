c41c413f89febe080511e97e67ce0e1e1047daa5
Natthakit <nonusae@gmail.com>
2017-08-12 14:33:05 +0700

    second commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'https://rubygems.org'
+ruby '2.2.3'
+
+gem 'rails', '4.1.1'
+gem 'sqlite3'
+
+gem 'sass-rails'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails', '~> 3.1.0'
+gem 'turbolinks', '~> 2.2.2'
+gem 'jbuilder', '~> 2.0'
+gem 'simple_form', '~> 3.1.0.rc1'
+gem 'bootstrap'
---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,41 @@
+class TasksController < ApplicationController
+  before_action :all_tasks, only: [:index, :create, :update, :destroy]
+  before_action :set_tasks, only: [:edit, :update, :destroy]
+  respond_to :html, :js
+
+
+  # def index
+  #   @tasks = Task.all
+  # end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+  end
+
+  def update
+    @task.update_attributes(task_params)
+  end
+
+  def destroy
+    @task.destroy
+  end
+
+
+  private
+
+    def all_tasks
+      @tasks = Task.all #this why we  remove index action
+    end
+
+    def set_tasks
+      @task = Task.find(params[:id])
+    end
+
+    def task_params
+      params.require(:task).permit(:description, :deadline)
+    end
+end
